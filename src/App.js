9b3d3578b59ae35fa6378127462c002f08332f91
songk1992-home <zuppiy@naver.com>
2021-04-08 23:25:53 +0900

    render() 방식으로 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +22,9 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    )
+
+  }
 }
 
 export default App;
