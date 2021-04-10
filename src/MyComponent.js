eca1ab59d2db3e0969374482e5ae904202528aab
songk1992-home <zuppiy@naver.com>
2021-04-10 10:50:36 +0900

    added tachyons library

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -4,7 +4,7 @@ import './main.css';
 class Hello extends Component {
     render(){
         return (
-        <div className="myClass">
+        <div className="f1 tc">
             <h1>Hello World</h1>
             <p>Welcome to React</p>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MyComponent from './MyComponent';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
