855221be95e2eeb4d55d915120e60dfa5b40c128
songk1992-home <zuppiy@naver.com>
2021-04-10 10:43:44 +0900

    My First Component with CSS

---
+++ src/MyComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './main.css';
+
+class Hello extends Component {
+    render(){
+        return (
+        <div className="myClass">
+            <h1>Hello World</h1>
+            <p>Welcome to React</p>
+        </div>
+        );
+
+
+    }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyComponent from './MyComponent';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
