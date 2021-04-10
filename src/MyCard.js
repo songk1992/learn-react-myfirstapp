1951759e4c95bbd7ec8b6e777fc83ab9dff9f81c
songk1992-home <zuppiy@naver.com>
2021-04-10 13:33:09 +0900

    learn basic properties 1

---
+++ src/MyCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({ name, email, id }) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='사진' src={`https://robohash.org/${id}?200x200`} />
+            <div><h2>{name}</h2></div>
+            <p>{email}</p>
+        </div>
+
+    );
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+
+    {
+        id : 1,
+        name: '김로봇',
+        username : '김씨',
+        email : 'kimc@example.com'
+    },
+    {
+        id : 2,
+        name: '박로봇',
+        username : '박씨',
+        email : 'parkc@example.com'
+    },
+    {
+        id : 3,
+        name: '이로봇',
+        username : '이씨',
+        email : 'leec@example.com'
+    },
+    {
+        id : 4,
+        name: '허로봇',
+        username : '허씨',
+        email : 'hush@example.com'
+    },
+    {
+        id : 5,
+        name: '리로봇',
+        username : '리씨',
+        email : 'leec2@example.com'
+    },
+    {
+        id : 6,
+        name: '로봇6',
+        username : '6씨',
+        email : '6c@example.com'
+    },
+    {
+        id : 7,
+        name: '로봇7',
+        username : '7씨',
+        email : '7c@example.com'
+    },
+    {
+        id : 8,
+        name: '서울봇',
+        username : '서울시',
+        email : 'seoulc@example.com'
+    },
+    {
+        id : 9,
+        name: '부산봇',
+        username : '부산시',
+        email : 'pusanc@example.com'
+    },
+    {
+        id : 10,
+        name: '제주봇',
+        username : '제주시',
+        email : 'jejuc@example.com'
+    }
+]
