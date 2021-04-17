cc70168b2c5fa39fde4c77ab1fa3aa0d81ce46d8
songk1992-home <zuppiy@naver.com>
2021-04-17 11:37:56 +0900

    React Hook 으로  변환

--- src/containers/MyApp.js
+++ src/containers/MyApp.js
@@ -1,54 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import MyCardList from '../components/MyCardList';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
 import ErrorBoundary from '../components/ErrorBoundary.js';
 import './MyCss.css';
 
-class App extends Component {
+function App() {
 
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
 
-    componentDidMount() {
+
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users=>this.setState({robots : users}));
+    // }
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users=>this.setState({robots : users}));
+        .then(users=> {setRobots(users)});
+    }, [])
 
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+    const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
 
-    render() {
-        const {robots, searchfield} = this.state;
-        const filteredRobots = robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
+    return !robots.length ? 
+    
+    <h1>Loading</h1> : 
+    (
+        <div className='outerBox'>
+        <span>로봇 친구들</span>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+            <ErrorBoundary>
+                <MyCardList robots={filteredRobots}/>
+            </ErrorBoundary>
 
-        return !robots.length ? 
-        
-        <h1>Loading</h1> : 
-        (
-            <div className='outerBox'>
-            <span>로봇 친구들</span>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-                <ErrorBoundary>
-                    <MyCardList robots={filteredRobots}/>
-                </ErrorBoundary>
+        </Scroll>
+        </div>
+    );
 
-            </Scroll>
-            </div>
-        );
-
-    }
+    
 }
 
 export default App;
